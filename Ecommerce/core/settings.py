9327e621fac9663a1befb9b9e43632d4053a2e42
maryam rezvani <rezvani.maryam.402@gmail.com>
2024-08-14 14:45:22 +0330

    adding media files

--- Ecommerce/core/settings.py
+++ Ecommerce/core/settings.py
@@ -119,10 +119,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+#static files
 STATIC_URL = 'static/'
 STATICFILES_DIR = [
     BASE_DIR / 'static'
 ]
+
+
+#media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT= BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- Ecommerce/home/models.py
+++ Ecommerce/home/models.py
@@ -1,4 +1,3 @@
-from tabnanny import verbose
 from django.db import models
 
 
@@ -19,7 +18,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='products/%Y/%m/%d/')
     description = models.TextField()
     price = models.IntegerField()
     available = models.BooleanField(default=True)
