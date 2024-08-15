d004654ff130accec5320e3674bdf0da643eb595
maryam rezvani <rezvani.maryam.402@gmail.com>
2024-08-15 09:47:49 +0330

    Fix datetime naive error

--- Ecommerce/accounts/models.py
+++ Ecommerce/accounts/models.py
@@ -33,7 +33,7 @@ class User(AbstractBaseUser):
 
 
 class OtpCode(models.Model):
-    phone_number = models.CharField(max_length=11)
+    phone_number = models.CharField(max_length=11, unique=True)
     code = models.PositiveSmallIntegerField()
     created = models.DateTimeField(auto_now_add=True)
 
