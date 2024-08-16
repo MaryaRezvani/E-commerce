ee27430035445f391edec65498acabdcb689fca0
maryam rezvani <rezvani.maryam.402@gmail.com>
2024-08-16 17:38:55 +0330

    Add order checkout

--- Ecommerce/orders/urls.py
+++ Ecommerce/orders/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('cart/', views.CartView.as_view(), name='cart'),
     path('cart/add/<int:product_id>/', views.CartAddView.as_view(), name='cart_add'),
     path('cart/remove/<int:product_id>/', views.CartRemoveView.as_view(), name='cart_remove'),
+    path('create/', views.OrderCreateView.as_view(), name='order_create'),
+    path('detail/<int:order_id>/',views.OrderDetailView.as_view(), name='order_detail'),
 ]
 
 
