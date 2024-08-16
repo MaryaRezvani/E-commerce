9f734ab64cc76690edc1a4221d2cc50df89be6e0
maryam rezvani <rezvani.maryam.402@gmail.com>
2024-08-16 14:30:13 +0330

    Add context processors to display the number of shopping carts

--- Ecommerce/orders/cart.py
+++ Ecommerce/orders/cart.py
@@ -24,6 +24,9 @@ class Cart:
             yield item
 
 
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
+
     def add(self, product, quantity):
         product_id = str(product.id)
         if product_id not in self.cart:
@@ -36,7 +39,7 @@ class Cart:
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
-            
+
 
     def save(self):
         self.session.modified = True
